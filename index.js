d562f60493e7b0251430e15e8dc5287078ff56d1
ggriffin-carecloud <ggriffin@carecloud.com>
2017-01-19 23:20:29 -0500

    claimed to escape but didnt escape whitespace? fixed

--- index.js
+++ index.js
@@ -2,10 +2,21 @@
 
 var matchOperatorsRe = /[|\\{}()[\]^$+*?.]/g;
 
-module.exports = function (str) {
-	if (typeof str !== 'string') {
-		throw new TypeError('Expected a string');
-	}
+var whitespace = /[\t\r\n\v]/
+var tb = /\t/g;
+var vt = /\v/g;
+var nl = /\n/g;
+var cr = /\r/g;
 
-	return str.replace(matchOperatorsRe, '\\$&');
+module.exports = function (str) {
+	if (!str.toString) return str;
+	var idx;
+	str = str.toString().replace(matchOperatorsRe, '\\$&');
+	if(str.search(whitespace) > -1){
+		str = str.replace(tb,'\\t');
+		str = str.replace(vt,'\\v');
+		str = str.replace(nl,'\\n');
+		str = str.replace(cr,'\\r');
+	}
+	return str;
 };
